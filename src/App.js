3042fcd442c8c1d7473355cd44c2f45803027b69
Harsh Tiwari <harsh.tiwari1995@gmail.com>
2021-10-17 14:13:16 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
     "A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠",
   ])
   const [flag, setFlag] = useState(false)
+  const [fcard1, setFcard1]= useState('')
+  const [fcard2, setFcard2]= useState('')
+  const [fcard3, setFcard3]= useState('')
+  const [fcard4, setFcard4]= useState('')
+  const [fcard5, setFcard5]= useState('')
 
 
   function handleClick(array) {
@@ -21,6 +26,23 @@ function App() {
   useEffect(() => {
     setFlag(!flag)
   }, [cards,flag])
+  function fetchCards(array){
+    let rand1  = (Math.floor(Math.random()*51)+1)
+    let rand2  = (Math.floor(Math.random()*51)+1)
+    let rand3  = (Math.floor(Math.random()*51)+1)
+    let rand4  = (Math.floor(Math.random()*51)+1)
+    let rand5  = (Math.floor(Math.random()*51)+1)
+
+
+    setFcard1(array.slice(rand1,rand1+1))
+    setFcard2(array.slice(rand2,rand2+1))
+    setFcard3(array.slice(rand3,rand3+1))
+    setFcard4(array.slice(rand4,rand4+1))
+    setFcard5(array.slice(rand5,rand5+1))
+    // console.log(fcard)
+    // console.log(fcard)
+    
+  }
   return (
     <>
       <div className="app">
@@ -33,7 +55,18 @@ function App() {
       </div>
       <div className="button">
         <button onClick={() => handleClick(cards)}>Shuffle</button>
-        <button>Fetch 5 cards</button>
+        <button onClick={()=>fetchCards(cards)}>Fetch 5 cards</button>
+      </div>
+      <div>
+        
+        <div id="fcard">
+        <p>{fcard1}</p>
+        <p>{fcard2}</p>
+        <p>{fcard3}</p>
+        <p>{fcard4}</p>
+        <p>{fcard5}</p>
+
+        </div>
       </div>
     </>
   );
